5a7b20c204d3fced662f16642ad57f9523e31695
arguelles <arguelles@wisc.edu>
2020-05-01 13:24:11 -0500

    improved read lic file

--- resources/example/read_lic.cpp
+++ resources/example/read_lic.cpp
@@ -20,23 +20,28 @@ int main(int argc, char ** argv) {
         throw std::runtime_error("usage: configure.lic");
 
     std::string configuration_filename(argv[1]);
-    std::cout << "single mode" << std::endl;
-    LW::RangeSimulationDetails rsd(configuration_filename);
-    std::cout << "multi mode" << std::endl;
     std::vector<std::shared_ptr<LW::Generator>> generators = LW::MakeGeneratorsFromLICFile(configuration_filename);
-    std::cout << "victory: " << generators.size() << std::endl;
+    std::cout << "Number of generators in LIC file: " << generators.size() << std::endl;
+    std::cout << std::endl;
 
+    unsigned int i = 1;
     for (auto g : generators){
+      std::cout << "Simulation details of generator " << i << std::endl;
       auto x = std::dynamic_pointer_cast<LW::RangeGenerator>(g)->GetSimulationDetails();
 
-      std::cout << x.Get_NumberOfEvents() << std::endl;
-      std::cout << x.Get_MinEnergy() << std::endl;
-      std::cout << x.Get_MaxEnergy() << std::endl;
-      std::cout << x.Get_MinZenith() << std::endl;
-      std::cout << x.Get_MaxZenith() << std::endl;
-      std::cout << x.Get_MinAzimuth() << std::endl;
-      std::cout << x.Get_MaxAzimuth() << std::endl;
-      std::cout << x.Get_PowerLawIndex() << std::endl;
+      std::cout << "Number of events per file:" << x.Get_NumberOfEvents() << std::endl;
+      std::cout << "Final State Particle 0:" << (int)x.Get_ParticleType0() << std::endl;
+      std::cout << "Final State Particle 1:" << (int)x.Get_ParticleType1() << std::endl;
+      std::cout << "Minimum injected energy:" << x.Get_MinEnergy() << std::endl;
+      std::cout << "Maximum injected energy:" << x.Get_MaxEnergy() << std::endl;
+      std::cout << "Minimum zenith angle (rad):" << x.Get_MinZenith() << std::endl;
+      std::cout << "Maximum zenith angle (rad):" << x.Get_MaxZenith() << std::endl;
+      std::cout << "Minimum azimith angle (rad):" << x.Get_MinAzimuth() << std::endl;
+      std::cout << "Maximum azimuth angle (rad):" << x.Get_MaxAzimuth() << std::endl;
+      std::cout << "Injection power-law spectral index:" << x.Get_PowerLawIndex() << std::endl;
+      std::cout << std::endl;
+
+      i++;
     }
 
     return 0;
